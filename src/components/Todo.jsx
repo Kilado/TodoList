6cdca96feb25a24947985dc0a7b918b76be60df2
Alex <ryzhenko.sasha@yandex.ru>
2023-05-08 14:48:12 +0300

    Added the ability to mark tasks as completed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,13 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default function Todo({ id, todo, deleteTodo }) {
+export default function Todo({ todo, deleteTodo, toggleComplete }) {
   return (
     <div className="Todo">
-      <p>{todo}</p>
+      <p
+        className={`${todo.isCompleted ? "completed" : ""}`}
+        onClick={() => toggleComplete(todo.id)}
+      >
+        {todo.todo}
+      </p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(id)} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(todo.id)} />
       </div>
     </div>
   );
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -8,11 +8,22 @@ export default function TodoWrapper() {
   const [todos, setTodos] = React.useState([]);
 
   const addTodo = (task) => {
-    setTodos([...todos, { id: uuidv4(), todo: task, isEditing: false }]);
+    setTodos([
+      ...todos,
+      { id: uuidv4(), todo: task, isCompleted: false, isEditing: false },
+    ]);
   };
 
   const deleteTodo = (id) => {
     setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo,
+      ),
+    );
   };
 
   return (
@@ -20,11 +31,13 @@ export default function TodoWrapper() {
       <h1>Get Thing Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo) => (
+        // <p>{console.log(todo.isCompleted)}</p>
         <Todo
           key={todo.id}
           id={todo.id}
-          todo={todo.todo}
+          todo={todo}
           deleteTodo={deleteTodo}
+          toggleComplete={toggleComplete}
         />
       ))}
     </div>
